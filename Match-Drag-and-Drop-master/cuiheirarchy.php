6cf412f2678c6068bf938db96c73c7e0cb960349
Hrishi29 <31528997+Hrishi29@users.noreply.github.com>
2019-06-27 13:43:11 -0400

    undo copy

--- Match-Drag-and-Drop-master/cuiheirarchy.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-            $file = 'grammarcat_cuing_heirarchy.json';
-            $data = file_get_contents($file);
-            echo $data;
-
-?>
--- Match-Drag-and-Drop-master/questions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-            $file = 'grammarcat_sentence_set2.json';
-            $data = file_get_contents($file);
-            echo $data;
-
-?>
